aa30cbe3f2b4a275b69e714393232debee27daeb
everettjf <everettjf@live.com>
2019-03-17 13:26:33 +0800

    fix sub item

--- src/provider.ts
+++ src/provider.ts
@@ -1,5 +1,17 @@
 import * as vscode from 'vscode';
 
+function getLeftSpaceWidth(text: string) {
+    let width = 0;
+    for (let i = 0; i < text.length; i+=1 ){
+        if (text.charAt(i) === ' ') {
+            width += 1;
+        } else {
+            break;
+        }
+    }
+    return width;
+}
+
 function readDocumentLines(document: vscode.TextDocument) {
     console.log('filename = ' + document.fileName);
 
@@ -9,21 +21,40 @@ function readDocumentLines(document: vscode.TextDocument) {
     }
 
     let inDependencyScope = false;
+    let lastItemOK = false;
+    let lastItemLeftSpaceWidth = 0;
 
     return new Array(document.lineCount).fill('')
     .map((line,idx) => document.lineAt(idx))
     .filter( line => {
         const {text} = line;
+        
+        // ignore empty line
+        if (text.trim().length === 0) {
+            return false;
+        }
 
-        // at lease one space
+        // at least one space
         if (text.startsWith(' ') && inDependencyScope) {
             if (!text.includes(':')) {
+                console.log('failed no: =',text);
                 return false;
             }
 
+            if (lastItemOK) {
+                let currentWidth = getLeftSpaceWidth(text);
+                if(currentWidth > lastItemLeftSpaceWidth) {
+                    // child item, ignore
+                    return false;
+                }
+            }
+
+            lastItemOK = true;
+            lastItemLeftSpaceWidth = getLeftSpaceWidth(text);
             return true;
         }
 
+        // detect dependency scope
         if (text.startsWith('dependencies:') && text.trim() === 'dependencies:') {
             inDependencyScope = true;
         } else if (text.startsWith('dev_dependencies:') && text.trim() === 'dev_dependencies:') {
@@ -31,6 +62,7 @@ function readDocumentLines(document: vscode.TextDocument) {
         } else {
             inDependencyScope = false;
         }
+        lastItemOK = false;
         return false;
     });
 }
@@ -38,7 +70,7 @@ function readDocumentLines(document: vscode.TextDocument) {
 export default class PubspecCodeLensProvider implements vscode.CodeLensProvider {
     provideCodeLenses(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.ProviderResult<vscode.CodeLens[]> {
         let lines = readDocumentLines(document);
-        console.log(lines.map((line,idx) => line.text));
+        // console.log(lines.map((line,idx) => line.text));
         return lines.map( line => {
             let packageName = line.text.trim().split(':')[0].trim();
 
