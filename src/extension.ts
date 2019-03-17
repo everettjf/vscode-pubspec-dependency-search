121ad5a96aad4364da1e017de5a40e396827e2d6
everettjf <everettjf@live.com>
2019-03-17 11:51:57 +0800

    basic search

--- src/extension.ts
+++ src/extension.ts
@@ -13,11 +13,20 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.pubspecDependencySearch', () => {
 		// The code you place here will be executed every time your command is executed
+		vscode.window.showInputBox().then( text => {
+			if (text === undefined || text === '') {
+				console.log('no input');
+				return;
+			}
+			console.log('input : ' + text);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+			// https://pub.dartlang.org/packages?q=path
+			let searchUrl = 'https://pub.dartlang.org/packages?q=' + encodeURI(text);
+			vscode.env.openExternal(vscode.Uri.parse(searchUrl));
+		});
+
 	});
 
 	context.subscriptions.push(disposable);
